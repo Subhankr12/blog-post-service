3252cc709cbaf4d13000c9ad76d3384fdd8af489
subhankarroy <subhankar.roy@edfora.com>
2021-12-11 14:05:50 +0530

    add api for updating user limit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const adminRoutes = require('../modules/admin/admin.routes');
 const authRoutes = require('../modules/auth/auth.routes');
 const blogRoutes = require('../modules/blog/blog.routes');
 
 const router = express.Router();
 
+router.use('/admin', adminRoutes);
 router.use('/auth', authRoutes);
 router.use('/blog', blogRoutes);
 
