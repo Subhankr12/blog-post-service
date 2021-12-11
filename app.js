7a1577f81e81a3f7e273d6f4c250ae65fb83102c
subhankarroy <subhankar.roy@edfora.com>
2021-12-11 13:21:21 +0530

    add postman url redirect

--- app.js
+++ app.js
@@ -6,12 +6,16 @@ const { DEFAULT_PORT } = require('./utils/constants');
 const validator = require('./utils/ajv');
 const routes = require('./routes/index');
 
+const POSTMAN_URL = 'https://www.postman.com/subhankr12/workspace/blog';
+
 const app = express();
 const ENV = process.env;
 const PORT = ENV.PORT ?? DEFAULT_PORT;
 
 app.use(express.json({ extended: true }));
 app.use(validator);
+
+app.get('/', (req, res) => res.redirect(POSTMAN_URL));
 app.use('/api', routes);
 
 (() => {
